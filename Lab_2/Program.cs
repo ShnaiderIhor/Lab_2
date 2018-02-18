0df456e3e19518485f5feb85744c6fbd7a1f38da
Ihor Shnaider <ihor_shnaider@outlook.com>
2018-02-18 19:36:03 +0200

    Initial

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,62 @@ namespace Lab_2
 {
     class Program
     {
+        private static Dictionary<char, char> letterMapping = new Dictionary<char, char>()
+       {
+           {'Щ', 'A' },
+           {'ь', 'd' },
+           {'Ю', 'B' },
+           {'Я', 'C' },
+       };
+
         static void Main(string[] args)
         {
+            Encrypt();
+            Decrypt();
+        }
+
+        public static void Encrypt()
+        {
+            var text = File.ReadAllText("Прізвище1.txt");
+
+
+            var result = new StringBuilder();
+
+            foreach (var letter in text)
+            {
+                if(letterMapping.Keys.Contains(letter))
+                {
+                    result.Append(letterMapping[letter]);
+                }
+                else
+                {
+                    result.Append((char)(letter + 3));
+                }
+            }
+
+            File.WriteAllText("Прізвище2.txt", result.ToString());
+
+        }
+
+        public static void Decrypt()
+        {
+            var text = File.ReadAllText("Прізвище2.txt");
+
+            var result = new StringBuilder();
+
+            foreach (var letter in text)
+            {
+                if (letterMapping.Values.Contains(letter))
+                {
+                    result.Append(letterMapping.FirstOrDefault(f => f.Value == letter).Key);
+                }
+                else
+                {
+                    result.Append((char)(letter - 3));
+                }
+            }
+
+            File.WriteAllText("Прізвище3.txt", result.ToString());
         }
     }
 }
